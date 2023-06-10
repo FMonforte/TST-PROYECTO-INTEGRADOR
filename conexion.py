83fb420976357ef9df21a5d688a7f1163710159e
Castelli_Nicolas <90008745+CastelliNicolas@users.noreply.github.com>
2023-06-09 23:49:48 -0300

    Agregar archivo conexion.py con la clase Conexion

---
+++ conexion.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+
+class Conexion:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+
+        if self.connection.is_connected():
+            print("Conexion Exitosa")
+
+    def disconnect(self):
+        self.connection.close()
+        print("Desconexion")
