af9614f180c3622933d81b95e035392e5ecf2282
FMonforte <fermonforte2004@gmail.com>
2023-06-11 15:14:56 -0300

    Base de datos Dispositivos

---
+++ Evidencia4_BD.sql
@@ -0,0 +1,13 @@
+use proyectoIntegradorV01;
+
+CREATE TABLE if not exists Dispositivos (Modelo varchar(20) not null, Numero_de_Serie varchar(25) not null, Direccion_de_Instalacion varchar(30),
+Fecha datetime, Coordenadas varchar(20), Estado varchar(20)
+);
+INSERT INTO dispositivos (Modelo, Numero_de_Serie, Direccion_de_Instalacion, Fecha, Coordenadas, Estado)
+values("ARDUINO", "AR202855NO", "CASTILLA 1700", '2023-01-01', 00000, "OPERATIVO");
+INSERT INTO dispositivos (Modelo, Numero_de_Serie, Direccion_de_Instalacion, Fecha, Coordenadas, Estado)
+values("RASPBERRY", " RP25038ry", "LEÓN 1350", '2023-02-02', 00001, "BAJA BATERÍA");
+INSERT INTO dispositivos (Modelo, Numero_de_Serie, Direccion_de_Instalacion, Fecha, Coordenadas, Estado)
+values("ESP32", "ESP3540032", "MURCIA 1500", '2023-03-03', 00002, "SIN CONEXIÓN")
+
+SELECT * FROM dispositivos;
