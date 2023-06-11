ec53a88ac37e6aca6e6a8bb1016d5342411d6062
Emilio A. Vera <101851669+evera8@users.noreply.github.com>
2023-06-11 13:09:54 -0300

    subo de nuevo tabla propietario la anterior estaba incompleta

--- Evidencia4/propietario.sql
+++ Evidencia4/propietario.sql
@@ -1,8 +1,14 @@
 use proyectoIntegradorV01;
 
-CREATE TABLE propietario (id MEDIUMINT NOT NULL AUTO_INCREMENT, Nombre CHAR(30) NOT NULL, Apellido CHAR(39), FechaNacimiento datetime, PRIMARY KEY (id));
+CREATE TABLE propietario (id MEDIUMINT NOT NULL AUTO_INCREMENT, Nombre varchar(30) NOT NULL, Apellido varchar(39), FechaNacimiento datetime, PRIMARY KEY (id));
      
-INSERT INTO Propietario ( Apellido, Nombre, FechaNacimiento) 
-Values (Vera, Emilio, '05-07-1990' );
+INSERT INTO propietario (Nombre,Apellido,FechaNacimiento)
+Values ('Emilio','Vera','1985-05-08');
+INSERT INTO propietario (Nombre,Apellido,FechaNacimiento)
+Values ('Jose','Gonzalez','1965-05-08');
+INSERT INTO propietario (Nombre,Apellido,FechaNacimiento)
+Values ('Pablo','Figueroa','1980-05-08');
+
+
 
 
